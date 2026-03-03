3d35c3a1f1a64870313de5305198494d623b93c1
Mouhamed Sow <mouhopere@gmail.com>
2026-03-03 18:34:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1237036681-3aca9",
+  "appId": "1:1085675585586:web:c358e605360246a44a3c2b",
+  "apiKey": "AIzaSyDxoat7rWnYU8ERApT1BY1R7IX8Vtrz6Uo",
+  "authDomain": "studio-1237036681-3aca9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1085675585586"
+};
